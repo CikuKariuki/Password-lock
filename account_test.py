d766825c01bc1622b99798e396893a8ccec4594c
Wanjiku <Wanjiku>
2019-04-07 17:23:17 +0300

    test_save_credentials created and ran; failed

--- account_test.py
+++ account_test.py
@@ -23,7 +23,6 @@ class TestUser(unittest.TestCase): #testcase checks for an expected result
 
         self.assertEqual(self.new_account.username,"James")
 
-
 class TestCredentials(unittest.TestCase): #testcase checks for an expected result
     """
     Test class that defines test cases for the behaviours of the user and credentials classes.
@@ -44,6 +43,13 @@ class TestCredentials(unittest.TestCase): #testcase checks for an expected resul
         """
 
         self.assertEqual(self.new_account.password,"12345678")
+    
+    def test_save_credentials(self):
+        """
+        test_save_user test case to test if user object is saved 
+        """
+        self.new_account.save_credentials() #save the user info
+        self.assertEqual(len(Credentials.account_list),1)
 
 if __name__ == '__main__':
     unittest.main()    
