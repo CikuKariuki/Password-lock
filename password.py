37350bab50345b619200ea311b28bc5d6db85ea3
Wanjiku <Wanjiku>
2019-04-07 15:14:10 +0300

    used __init__ method to define properties of user

--- password.py
+++ password.py
@@ -3,6 +3,14 @@ class User:
     Class that generates new instances of passwords
     """
 
+    def __init__(self,username):
+        """
+        __init__method helps define properties of objects
+       
+       Args:
+            username: Name associated with account
+        """
+        self.username = username
 
 
 
