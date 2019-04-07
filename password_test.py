4338595b59de6680b5660f0118597a5f50ee6296
Wanjiku <Wanjiku>
2019-04-07 15:47:28 +0300

    Added a TestCase class

--- password_test.py
+++ password_test.py
@@ -2,4 +2,12 @@ import unittest
 from password import User #importing the user class
 from password import Credentials #importing the credentials class
 
+class TestPassword(unittest.TestCase):
+    """
+    Test class that defines test cases for the behaviours of the user and credentials classes.
+
+    Arg:
+        unittest.TestCase: TestCase class is used to help in creating test cases
+    """
+
 
